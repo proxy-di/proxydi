e4ac560ad702601e0cfbddd3d595abbfa563f274
Yolziii <yolziii@gmail.com>
2025-03-02 04:38:51 +0200

    add tests for 100% coverage, comment unused eperimental code for now

--- src/proxy.constuctor.ts
+++ src/proxy.constuctor.ts
@@ -1,29 +1,23 @@
-import { findInjectableId } from './injectable';
-import { DependencyClass, DependencyId, IProxyDiContainer } from './types';
+import { DependencyId, IProxyDiContainer } from './types';
 
 export function makeConstructorDependencyProxy<T>(
     container: IProxyDiContainer,
-    dependencyId: DependencyId | DependencyClass<any>
+    dependencyId: DependencyId
 ): T {
-    let values: Record<string | symbol, any> = {};
-    let wasResolved = false;
-
-    const id =
-        typeof dependencyId === 'function'
-            ? findInjectableId(dependencyId)
-            : dependencyId;
+    // let values: Record<string | symbol, any> = {};
+    // let wasResolved = false;
 
     function getDependency() {
-        if (container.isKnown(id)) {
-            const dependency = container.resolve(id) as any;
+        if (container.isKnown(dependencyId)) {
+            const dependency = container.resolve(dependencyId) as any;
 
-            if (!wasResolved) {
-                for (const key in values) {
-                    dependency[key] = values[key];
-                }
-                values = {};
-                wasResolved = true;
-            }
+            // if (!wasResolved) {
+            //     for (const key in values) {
+            //         dependency[key] = values[key];
+            //     }
+            //     values = {};
+            //     wasResolved = true;
+            // }
 
             return dependency;
         } else {
@@ -35,25 +29,22 @@ export function makeConstructorDependencyProxy<T>(
         {},
         {
             get: function (target: any, prop: string | symbol, receiver: any) {
-                if ((target as any)[prop]) {
-                    return (target as any)[prop];
-                }
-                if (values[prop]) {
-                    return values[prop];
-                }
+                // if (values[prop]) {
+                //     return values[prop];
+                // }
 
                 const dependency = getDependency();
                 return Reflect.get(dependency, prop, receiver);
             },
 
             set: function (_target: any, prop: string | symbol, value: any) {
-                if (!container.isKnown(id)) {
-                    values[prop] = value;
-                    return true;
-                } else {
-                    const dependency = getDependency();
-                    return Reflect.set(dependency, prop, value);
-                }
+                // if (!container.isKnown(dependencyId)) {
+                //     values[prop] = value;
+                //     return true;
+                // } else {
+                const dependency = getDependency();
+                return Reflect.set(dependency, prop, value);
+                //}
             },
 
             has: function (_target: any, prop: string | symbol) {
--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
                 'vitest.config.ts',
                 'tests/',
                 'src/types.ts',
+                'docs/',
             ],
         },
     },
