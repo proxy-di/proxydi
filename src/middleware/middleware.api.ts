59e7b16e3f2d7e40e9b6d741b9adbd92e4945875
Yolziii <yolziii@gmail.com>
2025-03-25 06:25:46 +0200

    fix API (#18)

--- src/middleware/middleware.api.ts
+++ src/middleware/middleware.api.ts
@@ -13,21 +13,21 @@ export interface MiddlewareEvent {
     resolve: <T>(context: MiddlewareContext<T>) => MiddlewareContext<T>;
 }
 
-export interface MiddlewareResolver {
-    onResolve: <T>(context: MiddlewareContext<T>) => MiddlewareContext<T>;
+export interface MiddlewareResolver<T> {
+    onResolve: (context: MiddlewareContext<T>) => MiddlewareContext<T>;
 }
 
 /**
  * Describe the middleware that able to listen to the registering of a dependency in containers hierarchy
  */
 
-export interface MiddlewareRegistrator {
-    onRegister<T>(context: MiddlewareContext<T>): void;
+export interface MiddlewareRegistrator<T> {
+    onRegister(context: MiddlewareContext<T>): void;
 }
 /**
  * Describe the middleware that able to listen to the removing of a dependency in containers hierarchy
  */
 
-export interface MiddlewareRemover {
-    onRemove<T>(context: MiddlewareContext<T>): void;
+export interface MiddlewareRemover<T> {
+    onRemove(context: MiddlewareContext<T>): void;
 }
